527069c6d64c0f9a7b14b329c11510fecb6bcd25
XuanTruong-060103 <xuantruong.060103@gmail.com>
2026-04-20 12:02:07 +0700

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Pizza from './Pizza.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+} 
+
